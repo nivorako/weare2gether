708a30d831d9dfb3a0e602be1c22f0ee1cea7e5f
nivorako <nivo.rakoto@yahoo.fr>
2023-07-26 19:44:17 +0200

    set header drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { setDrawer } from "../features/drawerSlice";
 
-import { AppBar, Toolbar, Button, Drawer, Box, Divider } from "@mui/material";
+import { AppBar, Toolbar, Button, Drawer, Box, Divider, CssBaseline } from "@mui/material";
 import styled from '@emotion/styled'
 import { useMediaQuery } from '@mui/material';
 
+import Close from "../assets/closeBlack.svg";
 import Logo from "../assets/Logo.png";
 import User from "../assets/user4.svg";
+import User1 from "../assets/user3.svg"
 import ShopBskt from "../assets/shopBskt.svg";
+import ShoppingBskt from "../assets/shoppingBskt.svg"
 import Menu from "../assets/menuWhite.svg";
+
+/**
+ * 
+ * @returns {JSX element} element représentant le Header
+ */
 
 const Header = () => {
 
@@ -25,52 +33,109 @@ const Header = () => {
         dispatch(setDrawer());  
     };
 
+    const test = () => {
+        console.log("okok");
+    }
+
     return (
-        <AppBar 
-            position="static" 
-            sx={{
-                backgroundColor:"#cf3201",
-                height:"400px",
-                display:"flex",
-                flexDirection:"column",
-                justifyContent: "space-between"
-            }}
-        >
-            <Toolbar 
+        <>
+            <CssBaseline />
+            <AppBar 
+                position="static" 
                 sx={{
-                    marginTop:"1rem",
+                    backgroundColor:"#cf3201",
+                    height:"400px",
                     display:"flex",
-                    justifyContent:"space-between"
+                    flexDirection:"column",
+                    justifyContent: "space-between"
                 }}
             >
-                <HeaderLink >
-                    <img src={Logo} alt="logo" />
-                </HeaderLink> 
-                {
-                    isMobile ?  <HeaderMenu onClick={toggleDrawer}>
-                                    <img src={Menu} alt="menu" /> 
-                                </HeaderMenu> 
-                            :
-                   ( <HeaderNav >
+                <Toolbar 
+                    sx={{
+                        marginTop:"1rem",
+                        display:"flex",
+                        justifyContent:"space-between"
+                    }}
+                >
                     <HeaderLink >
-                        <Button variant="text" sx={{color:"white"}}>BLOG</Button>
-                    </HeaderLink>
-                    <HeaderLink >
-                        <Button variant="text" sx={{color:"white"}}>BLOG</Button>
-                    </HeaderLink>
-                    <HeaderLink >
-                        <img src={User} alt="user"/>
-                    </HeaderLink>
-                    <HeaderLink >
-                        <img src={ShopBskt} alt="user"/>
-                    </HeaderLink>
-                    </HeaderNav>)
-                }
-            </Toolbar>
-            <HeaderTitle>
-                salsa
-            </HeaderTitle>
-        </AppBar>
+                        <img src={Logo} alt="logo" />
+                    </HeaderLink> 
+                    {
+                        isMobile ?  (   <>
+                                            <HeaderMenu onClick={toggleDrawer}>
+                                                <img src={Menu} alt="menu" /> 
+                                            </HeaderMenu>
+                                            <Drawer 
+                                                open={drawer.drawer}  
+                                                anchor="right"
+                                                PaperProps={{
+                                                    sx: {
+                                                      width: "100%",  // Définir la largeur du drawer à 100%
+                                                      zIndex: "10", // Placer le drawer au-dessus du reste du contenu
+                                                    }
+                                                  }}
+                                            >
+                                                <Box 
+                                                    sx={{                                                      
+                                                        width:"100%",
+                                                        height:"100%",
+                                                        padding:"3rem",
+                                                        display:"flex",
+                                                        flexDirection:"column",
+                                                        justifyContent:"space-around"
+                                                    }}
+                                                >
+                                                    <Box 
+                                                        onClick={toggleDrawer}
+                                                        sx={{
+                                                            alignSelf:"flex-end",
+                                                            cursor:"pointer",
+                                                        }}
+                                                    >
+                                                        <img src={Close} alt="close drawer" />
+                                                    </Box>
+                                                    <HeaderLink >
+                                                        <Button variant="text" sx={{color:"black"}} onClick={test}>BLOG</Button>
+                                                    </HeaderLink>
+                                                    <Divider />
+                                                    <HeaderLink >
+                                                        <Button variant="text" sx={{color:"black"}} onClick={test}>CONNEXION</Button>
+                                                    </HeaderLink>
+                                                    <Divider />
+                                                    <HeaderLink >
+                                                        <img src={User1} alt="user"/>
+                                                    </HeaderLink>
+                                                    <Divider />
+                                                    <HeaderLink >
+                                                        <img src={ShoppingBskt} alt="panier"/>
+                                                    </HeaderLink>
+                                                    <Divider />
+                                                </Box>
+                                            </Drawer>
+                                        </>
+                                    ) 
+                                :
+                    ( <HeaderNav >
+                        <HeaderLink >
+                            <Button variant="text" sx={{color:"white"}}>BLOG</Button>
+                        </HeaderLink>
+                        <HeaderLink >
+                            <Button variant="text" sx={{color:"white"}}>CONNEXION</Button>
+                        </HeaderLink>
+                        <HeaderLink >
+                            <img src={User} alt="user"/>
+                        </HeaderLink>
+                        <HeaderLink >
+                            <img src={ShopBskt} alt="user"/>
+                        </HeaderLink>
+                        </HeaderNav>)
+                    }
+                </Toolbar>
+                <HeaderTitle>
+                    salsa
+                </HeaderTitle>
+            </AppBar>
+        </>
     )
 };
 
